95186d976dda298fd8a2c93e3b2b4cf17420f614
yampoudel <rahulpaudel2015@outlook.com>
2021-08-08 07:40:49 +1000

    Suppliers and product table added to the database

--- database/migrations/2021_08_07_000300_create_products_table.php
+++ database/migrations/2021_08_07_000300_create_products_table.php
@@ -15,6 +15,17 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string("product_name",100);
+            $table->text("product_description");
+            $table->string("product_image")->nullable();
+            $table->string("product_color");
+            $table->float("product_price",8,2);
+            $table->integer("product_starting_stock");
+            $table->integer("product_purchased");
+            $table->integer("product_total_stock");
+            $table->integer("product_shipped");
+            $table->unsignedInteger("supplier_id");
+            $table->foreign("supplier_id")->references('id') ->on ('suppliers');
             $table->timestamps();
         });
     }
